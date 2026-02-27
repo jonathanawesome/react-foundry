7379ede4b95c4ab34933612d2caab03af19899d7
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-02-26 18:32:26 -0600

    base config

--- apps/demo/foundry.config.ts
+++ apps/demo/foundry.config.ts
@@ -4,10 +4,4 @@ export default defineConfig({
   previews: "src/components/**/*.preview.tsx",
   port: 5173,
   title: "Demo Components",
-  theme: {
-    colors: {
-      dark: { brand: "50% 0.3 270" }, // purple-ish
-      light: { brand: "50% 0.3 270" },
-    },
-  },
 });
